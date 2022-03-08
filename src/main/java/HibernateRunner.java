4d58814ecb827155af7b5713d12760d609523a30
Malh1n <a.v.malhin@gmail.com>
2022-03-08 14:32:38 +0300

    1

--- src/main/java/HibernateRunner.java
+++ src/main/java/HibernateRunner.java
@@ -1,2 +1,51 @@
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import converter.BirthdayConverter;
+import entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.SQLException;
+
 public class HibernateRunner {
+
+    public static void main(String[] args) throws SQLException {
+//        BlockingDeque<Connection> pool = null;
+//        Connection connection = pool.take();
+//        SessionFactory
+
+//        Connection connection = DriverManager
+//                .getConnection("db.url", "db.username", "db.password");
+//        Session
+        Configuration configuration = new Configuration();
+//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+//        configuration.addAnnotatedClass(User.class);
+        configuration.addAttributeConverter(new BirthdayConverter());
+        configuration.registerTypeOverride(new JsonBinaryType());
+        configuration.configure();
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+//            User user = User.builder()
+//                    .username("ivan9@gmail.com")
+//                    .firstname("Ivan")
+//                    .lastname("Ivanov")
+//                    .info("""
+//                            {
+//                                "name": "Ivan",
+//                                "id": 25
+//                            }
+//                            """)
+//                    .birthDate(new Birthday(LocalDate.of(2000, 1, 19)))
+//                    .role(Role.ADMIN)
+//                    .build();
+//            session.delete(user);
+            User user1 = session.get(User.class,"ivan9@gmail.com");
+            User user2 = session.get(User.class,"ivan9@gmail.com");
+            user1.setLastname("Petrov");
+
+            session.getTransaction().commit();
+        }
+    }
 }
